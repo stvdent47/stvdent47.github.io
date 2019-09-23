158d565874d81a53b362c0bffdbcf835d54cd88b
Anton Rodionov <stvdent47@gmail.com>
2019-09-24 01:35:33 +0300

    moved create onclick to js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-
+document.getElementById('createBtn').addEventListener('click', function() {
     const newLi = document.createElement('li');
     const inputText = document.getElementById('taskInput').value;
     newLi.innerHTML = inputText;
-    
+
     const deleteBtn = document.createElement('button');
     deleteBtn.className = 'deleteBtn';
     deleteBtn.innerHTML = 'delete';
-    
-    //newLi.remove();
-    
+
     newLi.append(deleteBtn);
     myList.append(newLi);
-}
+});
