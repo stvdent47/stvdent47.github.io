519024b97b3675208b3e2cb0c424cc20e56752ee
Anton Rodionov <stvdent47@gmail.com>
2019-09-23 16:06:34 +0300

    created adding a new LI and delete button

--- script.js
+++ script.js
@@ -2,14 +2,4 @@ const createTask = () => {
     alert('You have successfylly created a task!');
 
     
-
-    var newItem = document.createElement("li");
-    var newText = document.createTextNode("text4"   );
-    newItem.appendChild(newText);
-    document.getElementById("myList").appendChild(newItem);
-
-    
-    var button = document.createElement("button");
-    button.innerHTML = "delete";
-    document.body.appendChild(button);
 }
